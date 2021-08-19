548f7cf924c264c1477fc3a2ea8b24ea56c1571e
Ivan Grishin <ktezik@yandex.ru>
2021-08-19 19:01:11 +0300

    Last commit! App completed

--- HellowWorld/ViewController.swift
+++ HellowWorld/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var halloWorldLabel: UILabel!
+    @IBOutlet var showTextButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        halloWorldLabel.isHidden = true
+        showTextButtonLabel.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showTextButtonAction() {
+        halloWorldLabel.isHidden.toggle()
+        
+        if halloWorldLabel.isHidden {
+            showTextButtonLabel.setTitle("Show Text", for: .normal)
+        } else {
+            showTextButtonLabel.setTitle("Hide Text", for: .normal)
+        }
+    }
+    
 }
 
